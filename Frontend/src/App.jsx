d36ed04286a5772ed1b571085785afa3e103a332
Shubham Kumar <shubhamkumar4664@hotmail.com>
2026-01-01 01:05:18 +0530

    add .env feature

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,8 @@ import {
   ChevronRight
 } from 'lucide-react';
 
-const API_URL = "http://localhost:5000/api";
+// const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 const USER_ID = "69554c84d10abfad047d2862";
 
 function App() {
